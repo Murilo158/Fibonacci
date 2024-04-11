b681377e331bc9fe3b46ad5d6167940b4ce49d89
Murilo158 <162125821+Murilo158@users.noreply.github.com>
2024-04-11 07:47:05 -0300

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+package candicional;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int n = 10; // Altere esse valor para o número de termos desejados
+	        System.out.println("Sequência de Fibonacci com " + n + " termos:");
+	        for (int i = 0; i < n; i++) {
+	            System.out.print(fibonacci(i) + " ");
+	        }
+	    }
+
+	    public static int fibonacci(int n) {
+	        if (n <= 1) {
+	            return n;
+	        } else {
+	            return fibonacci(n - 1) + fibonacci(n - 2);
+	        }    
+
+	}
+
+}
